0bebf6482126a9722c9d4288e220e49968220ced
Zameemaz3118 <zameemaz@gmail.com>
2025-02-21 08:24:36 +0100

    Ajout de log

--- server.js
+++ server.js
@@ -21,17 +21,30 @@ app.get("/", (req, res) => {
 // Route du proxy
 app.post("/proxy", async (req, res) => {
   try {
+    console.log("Requête reçue :", req.body); // Log des données reçues par le serveur
+    
     const apiUrl = "https://search-fel.centprod.com/v3/search-fel";
     
+    // Effectuer la requête à l'API distante
     const response = await axios.post(apiUrl, req.body, {
       headers: {
         "Content-Type": "text/xml; charset=utf-8",
       },
     });
 
+    // Log de la réponse de l'API distante
+    console.log("Réponse de l'API :", response.status, response.data);
+
+    // Vérifier si la réponse a du contenu
+    if (!response.data || response.status === 204) {
+      console.log("Aucune donnée renvoyée par l'API");
+      return res.status(204).send(); // Retourner un 204 si aucune donnée
+    }
+
     // Redirige la réponse reçue de l'API vers le client
     res.status(response.status).send(response.data);
   } catch (error) {
+    // Log des erreurs
     console.error("Erreur du Proxy:", error.message);
     res.status(500).json({ error: "Erreur lors de la requête proxy" });
   }
